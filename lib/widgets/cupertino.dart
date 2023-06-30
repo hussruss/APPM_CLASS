3e57ba65d9d45f0871c4bfdf606037a02b46633c
Huss Russ <hruss@stelatech.com.mx>
2023-06-29 19:49:15 -0600

    Semana 9

--- lib/widgets/cupertino.dart
+++ lib/widgets/cupertino.dart
@@ -6,6 +6,132 @@ class CupertinoDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp();
+    return CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: Icon(Icons.arrow_back_ios),
+          middle: Text('Cupertino App'),
+        ),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            CupertinoButton(
+              onPressed: () {
+                _showAlertDialog(context);
+              },
+              child: Text('Button'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CupertinoButton.filled(
+              onPressed: () {
+                _showActionSheet(context);
+              },
+              child: Text('Button'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CupertinoActivityIndicator(),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              child: CupertinoDatePicker(
+                initialDateTime: DateTime.now(),
+                mode: CupertinoDatePickerMode.date,
+                use24hFormat: true,
+                // This shows day of week alongside day of month
+                //showDayOfWeek: true,
+                // This is called when the user changes the date.
+                onDateTimeChanged: (DateTime newDate) {},
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CupertinoSlider(
+              min: 0,
+              max: 50,
+              value: 20,
+              onChanged: (double value) {},
+            )
+          ],
+        ),
+      ),
+    );
   }
+}
+
+void _showActionSheet(BuildContext context) {
+  showCupertinoModalPopup<void>(
+    context: context,
+    builder: (BuildContext context) => CupertinoActionSheet(
+      title: const Text('Title'),
+      message: const Text('Message'),
+      actions: <CupertinoActionSheetAction>[
+        CupertinoActionSheetAction(
+          /// This parameter indicates the action would be a default
+          /// default behavior, turns the action's text to bold text.
+          isDefaultAction: true,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Default Action'),
+        ),
+        CupertinoActionSheetAction(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Action'),
+        ),
+        CupertinoActionSheetAction(
+          /// This parameter indicates the action would perform
+          /// a destructive action such as delete or exit and turns
+          /// the action's text color to red.
+          isDestructiveAction: true,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Destructive Action'),
+        ),
+      ],
+    ),
+  );
+}
+
+void _showAlertDialog(BuildContext context) {
+  showCupertinoModalPopup<void>(
+    context: context,
+    builder: (BuildContext context) => CupertinoAlertDialog(
+      title: const Text('Alert'),
+      content: const Text('Proceed with destructive action?'),
+      actions: <CupertinoDialogAction>[
+        CupertinoDialogAction(
+          /// This parameter indicates this action is the default,
+          /// and turns the action's text to bold text.
+          isDefaultAction: true,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('No'),
+        ),
+        CupertinoDialogAction(
+          /// This parameter indicates the action would perform
+          /// a destructive action such as deletion, and turns
+          /// the action's text color to red.
+          isDestructiveAction: true,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Yes'),
+        ),
+      ],
+    ),
+  );
 }
