5542b35a34aa3bb018fd597e9c7bd54159a1ff0e
Huss Russ <hruss@stelatech.com.mx>
2023-07-19 11:47:31 -0600

    parte 1

--- lib/widgets/animatedBuilder.dart
+++ lib/widgets/animatedBuilder.dart
@@ -11,14 +11,80 @@ class AnimatedBuilderWidget extends StatefulWidget {
 
 class _AnimatedBuilderWidgetState extends State<AnimatedBuilderWidget>
     with SingleTickerProviderStateMixin {
-  
+  late Animation<int> _animation;
+  late AnimationController _animationController;
+  late Animation<double> _animationSize;
+  late Animation<Color?> _animationColor;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 8));
+    _animation = IntTween(begin: 0, end: 1000).animate(_animationController);
+    _animationSize =
+        Tween(begin: 100.0, end: 300.0).animate(_animationController);
+    _animationColor = ColorTween(begin: Colors.amber, end: Colors.blue)
+        .animate(_animationController);
+    // _animationController.addListener(() {
+    //   setState(() {});
+    // });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        
-      ],
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+          ),
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return Text(
+                _animation.value.toString(),
+                style: TextStyle(fontSize: 30),
+              );
+            },
+          ),
+          Text(
+            _animation.value.toString(),
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return Container(
+                width: _animationSize.value,
+                height: _animationSize.value,
+                color: _animationColor.value,
+              );
+            },
+          ),
+          ElevatedButton(
+              onPressed: () {
+                if (_animationController.status == AnimationStatus.completed) {
+                  _animationController.reset();
+                  _animationController.forward();
+                } else {
+                  _animationController.forward();
+                }
+              },
+              child: Text('Start'))
+        ],
+      ),
     );
   }
 }
