bc7ff24fa8f97b77d21047689566dfb3e91333b8
Huss Russ <hruss@stelatech.com.mx>
2023-08-08 18:44:17 -0600

    Image picker

--- lib/pages/image_picker.dart
+++ lib/pages/image_picker.dart
@@ -12,6 +12,7 @@ class ImagePickerScreen extends StatefulWidget {
 
 class _ImagePickerScreenState extends State<ImagePickerScreen> {
   XFile? imageFile;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +22,53 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [Colors.pink, Colors.purple],
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  height: 400,
+                  width: MediaQuery.of(context).size.width,
+                  child: imageFile != null
+                      ? Image.file(File(imageFile!.path))
+                      : Container(
+                          child: const Center(
+                            child: Text('No Image!!'),
+                          ),
+                        ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () async {
+                        final picker = ImagePicker();
+                        imageFile = await picker.pickImage(
+                            source: ImageSource.camera, imageQuality: 100);
+                        setState(() {});
+                      },
+                      backgroundColor: Colors.black,
+                      child: Icon(Icons.camera),
+                    ),
+                    FloatingActionButton(
+                      onPressed: () async {
+                        final picker = ImagePicker();
+                        imageFile = await picker.pickImage(
+                            source: ImageSource.gallery, imageQuality: 100);
+                        setState(() {});
+                      },
+                      backgroundColor: Colors.black,
+                      child: Icon(Icons.library_add),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
